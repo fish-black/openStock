181ff185044113c24ef68902a6ad93add687e1b9
lihuan <fzd0579@sina.com>
2020-06-06 13:59:04 +0800

    Init commit, added basic steps

---
+++ com/fishblack/open/stock/utils/StockUtils.py
@@ -0,0 +1,31 @@
+import baostock as bs
+import pandas as pd
+import os
+
+#### 登陆系统 ####
+lg = bs.login()
+# 显示登陆返回信息
+print('login respond error_code:'+lg.error_code)
+print('login respond  error_msg:'+lg.error_msg)
+
+stockP = "sz"
+stockCode = "002635"
+rs = bs.query_history_k_data_plus(stockP + "." + stockCode,
+    "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,tradestatus,pctChg,peTTM,pbMRQ,psTTM,pcfNcfTTM,isST",
+    start_date='2020-06-01', end_date='2020-06-05',
+    frequency="d", adjustflag="3") #frequency="d"取日k线，adjustflag="3"默认不复权
+print('query_history_k_data_plus respond error_code:'+rs.error_code)
+print('query_history_k_data_plus respond  error_msg:'+rs.error_msg)
+
+#### 打印结果集 ####
+data_list = []
+while (rs.error_code == '0') & rs.next():
+    # 获取一条记录，将记录合并在一起
+    data_list.append(rs.get_row_data())
+result = pd.DataFrame(data_list, columns=rs.fields)
+#### 结果集输出到csv文件 ####
+result.to_csv("C:\\Work\\Project\\Python\\openStock\\output\\data\\"+stockP+"_"+stockCode+"_data.csv", encoding="gbk", index=False)
+print(result)
+
+#### 登出系统 ####
+bs.logout()
